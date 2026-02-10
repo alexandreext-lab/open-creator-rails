1d48bf3715f72170d8f7b5b76815b8c20528a59b
rob1997 <robelgetnetrg@gmail.com>
2026-02-10 18:54:09 +0300

    test trial

--- test/AssetRegistry.t.sol
+++ test/AssetRegistry.t.sol
@@ -8,16 +8,19 @@ import {BaseTest} from "./Base.t.sol";
 contract AssetRegistryTest is BaseTest {
     AssetRegistry public assetRegistry;
 
+    uint256 internal constant SUBSCRIPTION_PRICE = 100000000;
+    uint256 internal constant DURATION = 3600;
+
     bytes32 internal constant ASSET_ID = keccak256(abi.encodePacked("asset_id"));
 
     function setUp() public override {
         super.setUp();
 
-        assetRegistry = new AssetRegistry();
+        assetRegistry = new AssetRegistry(70, 30);
     }
 
     function test_createAsset() public {
-        address asset = assetRegistry.createAsset(ASSET_ID, 100000000, address(gameToken), signer);
+        address asset = assetRegistry.createAsset(ASSET_ID, SUBSCRIPTION_PRICE, address(gameToken), signer);
         assertEq(IAsset(asset).getAssetId(), ASSET_ID);
         assertEq(asset, address(assetRegistry.assets(ASSET_ID)));
     }
@@ -35,9 +38,8 @@ contract AssetRegistryTest is BaseTest {
         
         address owner = signer;
         address spender = assetRegistry.getAsset(ASSET_ID);
-        uint256 duration = 3600;
-        uint256 value = IAsset(spender).getSubscriptionPrice(duration);
-        uint256 deadline = block.timestamp + duration;
+        uint256 value = IAsset(spender).getSubscriptionPrice(DURATION);
+        uint256 deadline = block.timestamp + DURATION;
 
         (uint8 v, bytes32 r, bytes32 s) = getPermit(owner, spender, value, deadline);        
 
@@ -66,5 +68,27 @@ contract AssetRegistryTest is BaseTest {
         test_createAsset();
         address asset = assetRegistry.getAsset(ASSET_ID);
         assertEq(assetRegistry.getSubscriptionPrice(ASSET_ID, 10), IAsset(asset).getSubscriptionPrice(10));
+    }
+
+    function test_updateFeeShare() public {
+        assetRegistry.updateCreatorFeeShare(80);
+        assetRegistry.updateRegistryFeeShare(20);
+        assertEq(assetRegistry.getCreatorFee(100000000), 80000000);
+        assertEq(assetRegistry.getRegistryFee(100000000), 20000000);
+    }
+
+    function test_feeSplit() public {
+        uint256 registryBalance = gameToken.balanceOf(assetRegistry.getOwner());
+        uint256 creatorBalance = gameToken.balanceOf(signer);
+        
+        test_subscribe();
+
+        uint256 value = SUBSCRIPTION_PRICE * DURATION;
+
+        uint256 creatorFee = assetRegistry.getCreatorFee(value);
+        uint256 registryFee = assetRegistry.getRegistryFee(value);
+
+        assertEq(gameToken.balanceOf(signer) - creatorBalance, creatorFee);
+        assertEq(gameToken.balanceOf(assetRegistry.getOwner()) - registryBalance, registryFee);
     }
 }
